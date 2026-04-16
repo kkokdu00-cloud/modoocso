c9283305e0c2b8779037238334086c427a546cc3
kkokdu00-cloud <kkokdu00@gmail.com>
2026-04-16 13:09:36 +0900

    Change iframe to script for page redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+export const dynamic = 'force-static'
+
 export default function Home() {
   return (
-    <iframe 
-      src="/index.html" 
-      style={{width:'100%', height:'100vh', border:'none'}}
-    />
+    <html>
+      <body>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.location.replace('/index.html');
+        `}} />
+      </body>
+    </html>
   )
 }
