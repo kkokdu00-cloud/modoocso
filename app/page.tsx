b688cbad7ff255c501c217cabca0cf92685fed24
kkokdu00-cloud <kkokdu00@gmail.com>
2026-04-16 13:13:56 +0900

    Remove inline script for fetching index.html
    Removed script fetching and writing HTML directly to the document.

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,7 @@ export default function Home() {
   return (
     <main dangerouslySetInnerHTML={{__html: `
       <script>
-        fetch('/index.html')
-          .then(r => r.text())
-          .then(html => {
-            document.open();
-            document.write(html);
-            document.close();
-          });
+        window.location.href = '/index.html';
       </script>
     `}} />
   )
