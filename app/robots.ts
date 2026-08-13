98cd705343033ae0c41565dbb7ef6559da36020c
kkokdu00-cloud <kkokdu00@gmail.com>
2026-08-13 00:10:06 +0000

    fix: robots.ts 사이트맵 도메인 오타 수정 (.com -> .kr)

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://www.modoocso.com/sitemap.xml',
+    sitemap: 'https://www.modoocso.kr/sitemap.xml',
   }
 }
